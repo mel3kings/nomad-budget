7475ad7c56e739c8bb55e47ccdb323e799a44b47
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-10-06 17:55:31 +0700

    fix: move out of lib

--- src/pages/api/health.js
+++ src/pages/api/health.js
@@ -1,8 +1,8 @@
 // https://nextjs.org/docs/pages/building-your-application/routing/api-routes
 // https://nextjs.org/docs/getting-started/project-structure
 
-import { CURRENCIES } from "@/pages/lib/utils";
-import MoneyObject from "../lib/money";
+import { CURRENCIES } from "@/lib/utils";
+import MoneyObject from "../../lib/money";
 
 export default async function handler(req, res) {
   // const audMoney = new MoneyObject(CURRENCIES.AUD, 100.0);
