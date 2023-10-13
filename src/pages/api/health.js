28fe54dd02d03813bd4c2005fe2b4fa2b60def33
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-10-13 19:00:47 +0700

    feat: test health

--- src/pages/api/health.js
+++ src/pages/api/health.js
@@ -1,9 +1,3 @@
-// https://nextjs.org/docs/pages/building-your-application/routing/api-routes
-// https://nextjs.org/docs/getting-started/project-structure
-
-import { GenerateText } from "./openai";
-
 export default async function handler(req, res) {
-  const response = await GenerateText("hello how are you");
-  res.status(200).json({ message: `${response}` });
+  res.status(200).json({ message: `Service is up` });
 }
