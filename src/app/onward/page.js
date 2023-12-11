45b9405bd37761604674a9736c9e0edd8fa6eb61
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-12-11 17:26:02 +0900

    feat: design finished

--- src/app/onward/page.js
+++ src/app/onward/page.js
@@ -1,24 +1,26 @@
 import React from "react";
 import { PlaneSVG } from "./planeSVG";
+import { ArrowRightSVG } from "./arrowRightSVG";
 
 const PDFViewer = () => {
   return (
     <div className="pt-10 bg-white h-screen relative h-[100vh] w-[100vw]">
       <div className="grid bg-white px-10">
-        <div className="row-span-1 text-2xl font-semibold">
-          04 JAN 2024 &gt; 04 JAN 2024 <span className="font-normal text-lg">TRIP TO</span> TOKYO
+        <div className="row-span-1 text-xl font-bold flex">
+          04 JAN 2024 <ArrowRightSVG /> 04 JAN 2024{" "}
+          <span className="font-normal text-sm flex items-end pl-2 pr-1 pb-0.5">TRIP TO</span> TOKYO
         </div>
-        <div className="row-span-1 border-t-2 border-black text-2xl font-semibold">
-          <span className="font-light text-xl">PREPARED FOR</span>
+        <div className="row-span-1 border-t border-black text-2xl font-semibold">
+          <span className="font-normal text-xl">PREPARED FOR</span>
         </div>
-        <div className="row-span-1 text-2xl font-semibold leading-4 ">TATLONGHARI/MELCHOR ROBLEDO MR.</div>
-        <div className="row-span-1 text-2xl font-semibold">TATLONGHARI/APRIL SOMBRIO MS.</div>
+        <div className="row-span-1 text-xl font-bold leading-4 ">TATLONGHARI/MELCHOR ROBLEDO MR.</div>
+        <div className="row-span-1 text-xl font-bold">TATLONGHARI/APRIL SOMBRIO MS.</div>
         <div className="row-span-1"> </div>
-        <div className="row-span-1 pt-12 text-xl font-light leading-2">RESERVATION CODE: WGV36O</div>
-        <div className="row-span-1 text-xl border-b border-black pb-10 font-light leading-4">
+        <div className="row-span-1 pt-12 text-xl font-normal leading-2">RESERVATION CODE: WGV36O</div>
+        <div className="row-span-1 text-xl border-b border-black pb-10 font-normal leading-4">
           AIRLINE RESERVATION CODE: WGV36O
         </div>
-        <div className="row-span-1 text-2xl font-semibold flex items-center">
+        <div className="row-span-1 text-xl font-bold flex items-center">
           <PlaneSVG />
           <span className="font-normal pr-2">DEPARTURE: </span>
           THURSDAY 04 JAN
@@ -44,37 +46,44 @@ const PDFViewer = () => {
             <div className="grid col-span-6 font-medium text-lg">
               <div className="grid grid-cols-2 grid-rows-2 border-t border-t-gray-200 border-b border-b-gray-200">
                 <div className="col-span-1 px-2 row-span-1 border-b border-dashed border-b-gray-300 font-medium">
-                  ICN <br />
-                  Seoul, KR
+                  <div className="leading-4 pt-4 font-normal text-sm">
+                    ICN <br />
+                    Seoul, KR
+                  </div>
                 </div>
                 <div className="col-span-1 px-2 row-span-1 border-b border-dashed border-b-gray-300 border-r border-r-gray-300">
-                  NRT
-                  <br />
-                  Tokyo, JP
+                  <div className="leading-4 pt-4 font-normal text-sm">
+                    NRT
+                    <br />
+                    Tokyo, JP
+                  </div>
                 </div>
-                <div className="col-span-1 px-2 row-span-1 border-dashed border-r border-r-gray-300">
-                  <span className="text-sm font-medium">Departing At:</span> <br />
+                <div className="col-span-1 px-2 row-span-1 border-dashed border-r border-r-gray-300 leading-5">
+                  <span className="text-sm font-normal">Departing At:</span> <br />
                   08:10
                   <br />
                   <div className="leading-3">
-                    <span className="text-sm font-medium">Terminal:</span>
+                    <span className="text-sm font-normal">Terminal:</span>
                     <br />
-                    <span className="text-sm font-medium">1</span>
+                    <span className="text-sm font-normal">1</span>
                   </div>
                 </div>
-                <div className="col-span-1 px-2 row-span-1 border-dashed border-r border-r-gray-300">
-                  <span className="text-sm font-medium">Arriving At:</span> <br />
+                <div className="col-span-1 px-2 row-span-1 border-dashed border-r border-r-gray-300 leading-5">
+                  <span className="text-sm font-normal">Arriving At:</span> <br />
                   10:33
                   <br />
                   <div className="leading-3">
-                    <span className="text-sm font-medium">Terminal:</span>
+                    <span className="text-sm font-normal">Terminal:</span>
                     <br />
-                    <span className="text-sm font-medium">3</span>
+                    <span className="text-sm font-normal">3</span>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="grid col-span-3 border-t border-t-gray-200 border-b border-b-gray-200 border-r border-r-gray-300 font-regular text-sm p-4 leading-5 whitespace-nowrap">
+            <div
+              className="grid col-span-3 border-t border-t-gray-200 border-b border-b-gray-200 border-r border-r-gray-300 
+            font-normal text-md p-4 leading-5 whitespace-nowrap"
+            >
               Aircraft: <br />
               <br />
               BOEING 737-800
@@ -89,17 +98,17 @@ const PDFViewer = () => {
           </div>
           <div className="row-span-1 pt-4">
             {" "}
-            <div className="grid grid-cols-8 font-regular gaps-1 text-lg">
+            <div className="grid grid-cols-8 font-normal gaps-1 text-md">
               <div className="grid col-span-5 bg-gray-100 border border-white p-1">Passenger Name:</div>
               <div className="grid col-span-2 bg-gray-100 border border-white p-1">Seats:</div>
               <div className="grid col-span-1 bg-gray-100 border border-white p-1">Booking </div>
             </div>
-            <div className="grid grid-cols-8 font-regular gaps-1 text-lg leading-4">
+            <div className="grid grid-cols-8 font-normal gaps-1 text-md leading-4">
               <div className="grid col-span-5 border border-white p-1">TATLONGHARI/MELCHOR ROBLEDO MR.</div>
               <div className="grid col-span-2 border border-white p-1">Check-in required</div>
               <div className="grid col-span-1 border border-white p-1">CONFIRMED </div>
             </div>
-            <div className="grid grid-cols-8 font-regular gaps-1 text-lg leading-2 border-b border-black pb-5">
+            <div className="grid grid-cols-8 font-normal gaps-1 text-md leading-2 border-b border-black pb-5">
               <div className="grid col-span-5 border border-white p-1">TATLONGHARI/APRIL SOMBRIO MS.</div>
               <div className="grid col-span-2 border border-white p-1">Check-in required</div>
               <div className="grid col-span-1 border border-white p-1">CONFIRMED </div>
